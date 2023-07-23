1345b1cb74fddc7068708c929a9c59748cc44fe5
nayem9b <razwanniam1@gmail.com>
2023-07-23 22:48:04 +0600

    authentication added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
+import { useSession } from "next-auth/react";
 import Head from "next/head";
 const HomePage = () => {
+  const { data: session } = useSession();
   return (
     <div>
       <Head>
         <title>Next Auth</title>
       </Head>
-      <h1 style={{ textAlign: "center", marginTop:"10%" }}>Welcome To Next Auth Home Page</h1>
+      <h1 style={{ textAlign: "center", marginTop: "10%" }}>
+        Welcome To Next Auth Home Page
+      </h1>
+      <h1>Welcome{session?.user?.name}</h1>
     </div>
   );
 };
